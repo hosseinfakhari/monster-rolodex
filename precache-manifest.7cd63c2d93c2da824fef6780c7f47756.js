9aefb355843114954e1679ddcf1aff0378dcdf9f
Hossein Fakhari <fakhari.hossein@gmail.com>
2020-08-20 20:32:10 +0430

    Updates

---
+++ precache-manifest.7cd63c2d93c2da824fef6780c7f47756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78328b1f91503dc839201a93dcf21991",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e6672404c6421db43b1f",
+    "url": "/monster-rolodex/static/css/main.91d566cc.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6672404c6421db43b1f",
+    "url": "/monster-rolodex/static/js/main.81d134bf.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
